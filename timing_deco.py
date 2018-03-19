212985f6efba38b65971a42b24818242ea3bb56f
SHalifaxK <34966355+SHalifaxK@users.noreply.github.com>
2018-03-19 13:21:18 +0200

    Create timing_deco.py

---
+++ timing_deco.py
@@ -0,0 +1,22 @@
+#decorator demo
+import time
+
+def time_it(func):
+
+    def wrapper(*args,**kwargs):
+        start = time.time()
+        result = func(*args,**kwargs)  
+        end = time.time()
+        print(func.__name__ + ' took ' + str(end-start) + ' to excecute')
+    return wrapper
+    
+@time_it        
+def loo():
+    d = range(0,1000)
+    time.sleep(3)
+    print(d)
+        
+
+        
+        
+loo()
